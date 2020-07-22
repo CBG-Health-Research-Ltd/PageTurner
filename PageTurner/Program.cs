d8f3264e3d74871739b77a3271b8c66cc28acc57
Jack Gribben <jack.gribben@cbg.co.nz>
2020-07-22 14:09:22 +1200

    Add project files.

---
+++ PageTurner/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Timers;
+using System.Threading;
+
+//Splits the string recieved by TSS and then adds relevant question adress to a .txt file.
+
+
+namespace PageTurner
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            
+            DateTime currentTimeMS = DateTime.Now;
+            string Username = Environment.UserName; //Get current user
+            string arg0 = null; 
+            string arg1 = null; //arbritrary showcard ID format.
+            string arg2 = null;
+            if (args.Length > 0) { arg0 = args[0]; }
+            if (args.Length > 1) { arg1 = args[1]; }
+            if (args.Length > 2) { arg2 = args[2]; }
+            string check = "Arg1 = " + arg0 + " and Arg2 = " + arg1; //The string containing PageTurner.exe arguments.
+            string Arguments = arg0 + " " + arg1 + " " + arg2;
+            var myFile = File.Create(@"C:\nzhs\questioninformation\QuestionLog\" + Arguments + ".txt");
+            myFile.Close();
+           
+        }
+
+    }
+
+}
